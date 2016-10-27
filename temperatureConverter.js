b4a9ff2078f9b4ac1ba8739fd766df28ecb1b462
Irene <irene.penuelas445@west-mec.org>
2016-10-27 14:16:50 -0700

    fixed loop

--- temperatureConverter.js
+++ temperatureConverter.js
@@ -7,8 +7,8 @@ function calculate(temp) {
     var result;
     var celsius = document.getElementById('inputCelsius');
     var fahrenheit = document.getElementById('inputFahrenheit');
-    var inputs = document.getElementsByClassName(inputsTemp);
-
+    var inputs = document.getElementsByClassName('inputsTemp');
+try {
     if (temp === "inputFahrenheit") {
         result = ( fahrenheit.value - 32) / 1.8;
         celsius.value = Math.round(result);
@@ -17,10 +17,12 @@ function calculate(temp) {
     else {
         result = celsius.value * 1.8 + 32;
         fahrenheit.value = Math.round(result);
+    } }
+catch (error) {
+        alert("input a number!");
     }
-    for (inputs; isNaN(inputs) || ""; inputs++) {
-       alert("Please enter a numeric value");
-        break;
-    } //end loop
+if (isNaN(result) || "") {
+    alert("That's NOT a number!")
+}
 
 }//function end
